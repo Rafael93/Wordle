0cca88ad7fb484444d18037f1db0dc7af8c8b94f
Rafael93 <rafaelgamas@gmail.com>
2023-09-13 18:59:42 -0400

    algoritm implemented and styles

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const input = document.getElementById("guess-input");
 const valor = input.value;
 const grid = document.getElementById("grid");
+const colores = {
+  verde: "green",
+  amarrillo: "rgb(247, 230, 98)",
+  gris: "rgb(224, 224, 224)",
+};
 let intentos = 6;
 let palabra = "APPLE";
 
@@ -25,6 +30,7 @@ function crearTablero() {
 }
 
 function intentar() {
+  const casillas = document.querySelectorAll(".square");
   const INTENTO = leerIntento();
   if (INTENTO === palabra) {
     console.log("GANASTE!");
@@ -33,13 +39,29 @@ function intentar() {
   for (let i in palabra) {
     if (INTENTO[i] === palabra[i]) {
       console.log(INTENTO[i], "VERDE");
+      const contenedorLetra = document.createElement("div");
+      contenedorLetra.className = "letter-container";
+      contenedorLetra.style.backgroundColor = colores.verde;
+      contenedorLetra.innerHTML = INTENTO[i];
+      casillas[i].appendChild(contenedorLetra);
     } else if (palabra.includes(INTENTO[i])) {
       console.log(INTENTO[i], "AMARILLO");
+      const contenedorLetra = document.createElement("div");
+      contenedorLetra.className = "letter-container";
+      contenedorLetra.style.backgroundColor = colores.amarrillo;
+      contenedorLetra.innerHTML = INTENTO[i];
+      casillas[i].appendChild(contenedorLetra);
     } else {
       console.log(INTENTO[i], "GRIS");
+      const contenedorLetra = document.createElement("div");
+      contenedorLetra.className = "letter-container";
+      contenedorLetra.style.backgroundColor = colores.gris;
+      contenedorLetra.innerHTML = INTENTO[i];
+      casillas[i].appendChild(contenedorLetra);
     }
   }
   intentos--;
+
   if (intentos == 0) {
     console.log("PERDISTE!");
   }
